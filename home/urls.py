7a888d12f53798251f9d02ff6a3cfcb2a4b74669
Harshil <harshil58558@gmail.com>
2024-08-30 11:30:14 +0530

    updated html and css

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("login_page", views.login, name="login_page"),
+    path("register_page", views.register, name="register_page"),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse("Hello, world. You're at the loans index.")
     return render(request,"home/home.html")
+
+def login(request):
+    # return HttpResponse("Hello, world. You're at the loans index.")
+    return render(request,"home/login.html")
+
+def register(request):
+    # return HttpResponse("Hello, world. You're at the loans index.")
+    return render(request,"home/register.html")
