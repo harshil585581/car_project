a08754f0055181e0d6585b90de48445f7e2cc39a
harshil <harshil58558@gmail.com>
2024-09-07 16:29:59 +0530

    deleted

--- home/urls.py
+++ home/urls.py
@@ -5,9 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login_page", views.login, name="login_page"),
     path("register_page", views.register, name="register_page"),
-    path("login_check",views.login_check.as_view(),name="login_check"),
-
-    # path("create_signup", views.create_signup.as_view(),name="create_signup"),
-    # path("signup_view", views.signup_view.as_view(),name="signup_view"),
-    # path("delete_register", views.delete_register.as_view(),name="delete_register")
+   
 ]
--- home/views.py
+++ home/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.views import APIView
 from django.http import JsonResponse
-from home.models import signup_check
 
 def home(request):
     # return HttpResponse("Hello, world. You're at the loans index.")
@@ -16,47 +15,5 @@ def register(request):
     # return HttpResponse("Hello, world. You're at the loans index.")
     return render(request,"home/register.html")
 
-class login_check(APIView):
-    def post(self,request):
-        username = request.POST['username']
-        password = request.POST['pass']
-        if username == 'harshil':
-            return JsonResponse({"status":"pass"})
-        else:
-            return JsonResponse({"status":"fail"})
+
         
-
-
-# class create_signup(APIView):
-#     def post(self, request):
-#         name = request.POST['name']
-#         cmob = request.POST['cmob']
-#         age = request.POST['age']
-#         usr = signup_check()
-#         usr.name = name
-#         usr.cmob = cmob
-#         usr.age = age
-#         usr.save()
-#         # print(username)
-#         # print(email)
-#         # print(password)
-#         # print(utype)
-#         return JsonResponse({"status": "pass"})
-
-# from django.views.generic import TemplateView
-
-# class signup_view(TemplateView):
-#     template_name = "view_signup.html"
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         user_data = signup_check.objects.all()
-#         context = { 'userdata': user_data}
-#         return context
-    
-
-# class delete_register(APIView):
-#     def post(self, request):
-#         cmob = request.POST['cmob']
-#         signup_check.objects.filter(cmob=cmob).delete()
-#         return JsonResponse({"status": "pass"})
