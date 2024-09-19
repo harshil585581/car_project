e509a459833aafd4a0e591ad67143671660b27d8
harshil <harshil58558@gmail.com>
2024-09-19 22:42:53 +0530

    updated cprofile

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login_page", views.login, name="login_page"),
     path("register_page", views.register, name="register_page"),
-     path("cprofile", views.cprofile, name="cprofile"), 
+    path("cprofile", views.cprofile.as_view(), name="cprofile"), 
 
     path('logout_view', views.logout_view, name='logout_view'),
    
--- home/views.py
+++ home/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 from django.http import JsonResponse
 from adminDash.models import reg_check
 from django.contrib.auth import logout
+from django.views.generic import TemplateView
+
 
 def home(request):
     user_data = reg_check.objects.all()
@@ -32,8 +34,39 @@ def register(request):
     return render(request,"home/register.html")
 
 def cprofile(request):
-    # return HttpResponse("Hello, world. You're at the loans index.")
-    return render(request,"home/cprofile.html")
+    user_data = reg_check.objects.all()
+    
+    # Get the current user from the session
+    current_user = request.session.get("user_data", "Guest")
+    
+    # Prepare context
+    context = {
+        'userdata': user_data,
+        'currentuser': current_user
+    }
+    
+    # Print the current user (for debugging purposes)
+    print("***********:request ", current_user)
+    
+    # Render the template with the context
+    return render(request,"home/cprofile.html", context)
+
+
+
+class cprofile(TemplateView):
+    template_name = "home/cprofile.html" 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = self.request.session.get("user_data")
+        user = reg_check.objects.get(name=username)
+        print(user.password)
+        context['currentuser'] = user.name
+        context['mobile'] = user.cmob
+        context['password'] = user.password
+        return context
+
+
 
 def logout_view(request):
     logout(request)
